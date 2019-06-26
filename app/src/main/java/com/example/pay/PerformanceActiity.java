8f014c2168a10f664fd0e6e33fe91ff768a99ee7
Yaseen <myaseen2030@gmail.com>
2019-06-26 14:11:44 +0500

    6/26/2019

--- app/src/main/java/com/example/pay/PerformanceActiity.java
+++ app/src/main/java/com/example/pay/PerformanceActiity.java
@@ -1,29 +1,47 @@
 package com.example.pay;
 
 
-import android.content.Context;
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.view.ViewGroup;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import com.example.pay.Adapter.FinanceAdapter;
+import com.example.pay.Model.User;
 
+import java.util.ArrayList;
 
 public class PerformanceActiity extends AppCompatActivity {
 
     ListView perItems, learnItems, orgItems, cusItems;
-    private ArrayAdapter perItemAdapter, learnItemAdapter, cusItemAdapter;
-
-
-
+    ArrayList<User> perNames;
+    FinanceAdapter perItemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_performance_actiity);
+
+        //create User Object
+        User item1 = new User("Increase profitability","120.00");
+        User item2 = new User("Improve HR workforce efficiancy \n"+ "through automation","120.00");
+        User item3 = new User("Increase profitability","120.00");
+        User item4 = new User("Increase profitability","120.00");
+
+        User learnItem1 = new User("Increase profitability","120.00");
+        User learnItem2 = new User("Increase profitability","120.00");
+        User learnItem3 = new User("Increase profitability","120.00");
+        User learnItem4 = new User("Increase profitability","120.00");
+
+        User cusItem1 = new User("Provide learning materials to all","120.00");
+        User cusItem2 = new User("Teach profitability","120.00");
+        User cusItem3 = new User("Increase profitability","120.00");
+        User cusItem4 = new User("Increase profitability","120.00");
 
         perItems = (ListView) findViewById(R.id.lvPerItems);
         learnItems = (ListView) findViewById(R.id.lvLearnItems);
@@ -31,39 +49,32 @@ public class PerformanceActiity extends AppCompatActivity {
         cusItems = (ListView) findViewById(R.id.lvCusItems);
 
 
-        ArrayList<String> perNames = new ArrayList<>();
-        perNames.add("Increase profitability");
-        perNames.add("Improve HR workforce efficiancy \n" +
-                     "through automation");
-        perNames.add("Increase profitability");
-        perNames.add("Increase profitability");
+        perNames = new ArrayList<>();
+        perNames.add(item1);
+        perNames.add(item2);
+        perNames.add(item3);
+        perNames.add(item4);
 
-        ArrayList<String> learnNames = new ArrayList<>();
-        learnNames.add("Increase profitability");
-        learnNames.add("Increase profitability");
-        learnNames.add("Increase profitability");
-        learnNames.add("Increase profitability");
+        ArrayList<User> learnNames = new ArrayList<>();
+        learnNames.add(learnItem1);
+        learnNames.add(learnItem2);
+        learnNames.add(learnItem3);
+        learnNames.add(learnItem4);
 
-        ArrayList<String> cusNames = new ArrayList<>();
-        cusNames.add("Provide learning materials to all");
-        cusNames.add("Teach profitability");
-        cusNames.add("Increase profitability");
-        cusNames.add("Increase profitability");
+        ArrayList<User> cusNames = new ArrayList<>();
+        cusNames.add(cusItem1);
+        cusNames.add(cusItem2);
+        cusNames.add(cusItem3);
+        cusNames.add(cusItem4);
 
-
-        perItemAdapter = new ArrayAdapter(this,R.layout.performance_list_view,R.id.tvPerList,perNames);
+        perItemAdapter = new FinanceAdapter(this,R.layout.performance_list_view,perNames);
         perItems.setAdapter(perItemAdapter);
 
-
-//        perItemAdapter = new ArrayAdapter(this,R.layout.performance_list_view,R.id.tvPerList,perNames);
-//        perItems.setAdapter(perItemAdapter);
-
-        learnItemAdapter = new ArrayAdapter(this,R.layout.performance_list_view,R.id.tvPerList,learnNames);
+        FinanceAdapter learnItemAdapter = new FinanceAdapter(this,R.layout.performance_list_view,learnNames);
         learnItems.setAdapter(learnItemAdapter);
         orgItems.setAdapter(learnItemAdapter);
 
-        cusItemAdapter = new ArrayAdapter(this,R.layout.performance_list_view,R.id.tvPerList,cusNames);
+        FinanceAdapter cusItemAdapter = new FinanceAdapter(this,R.layout.performance_list_view,cusNames);
         cusItems.setAdapter(cusItemAdapter);
-
     }
 }
